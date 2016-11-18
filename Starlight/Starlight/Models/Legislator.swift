d2d2b8bf097558dd8c6b1d72742d12114bf83b62
markedwardmurray <markedwardmurray@gmail.com>
2016-11-17 22:55:47 -0500

    Models | initialize Legislator with result JSON

--- Starlight/Starlight/Models/Legislator.swift
+++ Starlight/Starlight/Models/Legislator.swift
@@ -7,59 +7,44 @@
 //
 
 import Foundation
-
-enum Chamber : String {
-    case senate, house
-}
-
-typealias Fax = String
-typealias Phone = String
-enum Gender : String {
-    case M, F
-}
-enum Party : String {
-    case R, D, I
-}
-enum StateRank : String {
-    case junior, senior
-}
+import SwiftyJSON
 
 struct Legislator {
     let bioguide_id: String?
-    let birthday: Date?
-    let chamber: Chamber
+//    let birthday: Date?
+    let chamber: String
     let contact_form: String?
     let crp_id: String?
     let district: Int?
-    let fax: Fax?
-    let fec_ids: [String]
+    let fax: String?
+//    let fec_ids: [String]
     let first_name: String
-    let gender: Gender
+    let gender: String
     let govtrack_id: String?
-    let icpsr_id: Int?
+    let icpsr_id: String?
     let in_office: Bool
     let last_name: String
-    let lis_id: String
+    let lis_id: String?
     let middle_name: String?
     let name_suffix: String?
     let nickname: String?
-    let oc_email: String
-    let ocd_id: String
+    let oc_email: String?
+    let ocd_id: String?
     let office: String
-    let party: Party
-    let phone: Phone
-    let senate_class: Int
+    let party: String
+    let phone: String
+    let senate_class: Int?
     let state: String
     let state_name: String
-    let state_rank: StateRank
-    let term_end: Date
-    let term_start: Date
-    let thomas_id: String
+    let state_rank: String
+//    let term_end: Date
+//    let term_start: Date
+    let thomas_id: String?
     let title: String
-    let twitter_id: String
-    let votesmart_id: Int
-    let website: String
-    let youtube_id: String
+    let twitter_id: String?
+    let votesmart_id: String?
+    let website: String?
+    let youtube_id: String?
     
     var fullName: String {
         var fullName = title + ". " + first_name
@@ -74,87 +59,41 @@ struct Legislator {
         return fullName
     }
     
-    init(bioguide_id: String?, birthday: Date?, chamber: Chamber, contact_form: String?,
-         crp_id: String?, district: Int?, fax: Fax?, fec_ids: [String], first_name: String,
-         gender: Gender, govtrack_id: String?, icpsr_id: Int?, in_office: Bool,
-         last_name: String, lis_id: String, middle_name: String?, name_suffix: String?,
-         nickname: String?, oc_email: String, ocd_id: String, office: String, party: Party,
-         phone: Phone, senate_class: Int, state: String, state_name: String,
-         state_rank: StateRank, term_end: Date, term_start: Date, thomas_id: String,
-         title: String, twitter_id: String, votesmart_id: Int, website: String,
-         youtube_id: String) {
-        self.bioguide_id = bioguide_id
-        self.birthday = birthday
-        self.chamber = chamber
-        self.contact_form = contact_form
-        self.crp_id = crp_id
-        self.district = district
-        self.fax = fax
-        self.fec_ids = fec_ids
-        self.first_name = first_name
-        self.gender = gender
-        self.govtrack_id = govtrack_id
-        self.icpsr_id = icpsr_id
-        self.in_office = in_office
-        self.last_name = last_name
-        self.lis_id = lis_id
-        self.middle_name = middle_name
-        self.name_suffix = name_suffix
-        self.nickname = nickname
-        self.oc_email = oc_email
-        self.ocd_id = ocd_id
-        self.office = office
-        self.party = party
-        self.phone = phone
-        self.senate_class = senate_class
-        self.state = state
-        self.state_name = state_name
-        self.state_rank = state_rank
-        self.term_end = term_end
-        self.term_start = term_start
-        self.thomas_id = thomas_id
-        self.title = title
-        self.twitter_id = twitter_id
-        self.votesmart_id = votesmart_id
-        self.website = website
-        self.youtube_id = youtube_id
-    }
-    
     init(result: JSON) {
-        self.bioguide_id  = result["bioguide_id"]
-        self.birthday     = result["birthday"]
-        self.chamber      = result["chamber"]
-        self.contact_form = result["contact_form"]
-        self.crp_id       = result["crp_id"]
-        self.district     = result["district"]
-        self.fax          = result["fax"]
-        self.fec_ids      = result["fec_ids"]
-        self.first_name   = result["first_name"]
-        self.gender       = result["gender"]
-        self.govtrack_id  = result["govtrack_id"]
-        self.icpsr_id     = result["icpsr_id"]
-        self.in_office    = result["in_office"]
-        self.last_name    = result["last_name"]
-        self.lis_id       = result["lis_id"]
-        self.middle_name  = result["middle_name"]
-        self.name_suffix  = result["name_suffix"]
-        self.nickname     = result["nickname"]
-        self.oc_email     = result["oc_email"]
-        self.ocd_id       = result["ocd_id"]
-        self.office       = result["office"]
-        self.party        = result["party"]
-        self.phone        = result["phone"]
-        self.senate_class = result["senate_class"]
-        self.state        = result["state"]
-        self.state_name   = result["state_name"]
-        self.state_rank   = result["state_rank"]
-        self.term_end     = result["term_end"]
-        self.term_start   = result["term_start"]
-        self.thomas_id    = result["thomas_id"]
-        self.title        = result["title"]
-        self.twitter_id   = result["twitter_id"]
-        self.votesmart_id = result["votesmart_id"]
-        self.website      = result["website"]
-        self.youtube_id   = result["youtube_id"]
+        self.bioguide_id  = result["bioguide_id"].string
+//        self.birthday     = result["birthday"]
+        self.chamber      = result["chamber"].string!
+        self.contact_form = result["contact_form"].string
+        self.crp_id       = result["crp_id"].string
+        self.district     = result["district"].int
+        self.fax          = result["fax"].string
+//        self.fec_ids      = result["fec_ids"].array
+        self.first_name   = result["first_name"].string!
+        self.gender       = result["gender"].string!
+        self.govtrack_id  = result["govtrack_id"].string
+        self.icpsr_id     = result["icpsr_id"].string
+        self.in_office    = result["in_office"].bool!
+        self.last_name    = result["last_name"].string!
+        self.lis_id       = result["lis_id"].string
+        self.middle_name  = result["middle_name"].string
+        self.name_suffix  = result["name_suffix"].string
+        self.nickname     = result["nickname"].string
+        self.oc_email     = result["oc_email"].string
+        self.ocd_id       = result["ocd_id"].string
+        self.office       = result["office"].string!
+        self.party        = result["party"].string!
+        self.phone        = result["phone"].string!
+        self.senate_class = result["senate_class"].int
+        self.state        = result["state"].string!
+        self.state_name   = result["state_name"].string!
+        self.state_rank   = result["state_rank"].string!
+//        self.term_end     = result["term_end"]
+//        self.term_start   = result["term_start"]
+        self.thomas_id    = result["thomas_id"].string
+        self.title        = result["title"].string!
+        self.twitter_id   = result["twitter_id"].string
+        self.votesmart_id = result["votesmart_id"].string
+        self.website      = result["website"].string
+        self.youtube_id   = result["youtube_id"].string
     }
 }
