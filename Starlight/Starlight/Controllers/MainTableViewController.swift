6523ec3269519e5ebb438a3c2f5fd3927dff4b67
markedwardmurray <markedwardmurray@gmail.com>
2016-11-27 00:40:53 -0500

    load toolbar with saved legislators

--- Starlight/Starlight/Controllers/MainTableViewController.swift
+++ Starlight/Starlight/Controllers/MainTableViewController.swift
@@ -61,6 +61,12 @@ class MainTableViewController: UITableViewController, UISearchBarDelegate {
         self.loadUpcomingBills()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.loadToolbar(legislators: self.legislators)
+    }
+    
     func segmentedControlValueChanged(sender: UISegmentedControl) {
         self.segmentIndex = SegmentIndex(rawValue: sender.selectedSegmentIndex)!
         sender.tintColor = UIColor.white
@@ -303,6 +309,8 @@ class MainTableViewController: UITableViewController, UISearchBarDelegate {
                 case .error:
                     self.showAlertWithTitle(title: "Error!", message: "Failed to save your legislators")
                 case .success:
+                    self.loadToolbar(legislators: legislators)
+                    self.tableView.reloadData()
                     break
                 }
                 
@@ -365,6 +373,44 @@ class MainTableViewController: UITableViewController, UISearchBarDelegate {
                 })
             }
         })
+    }
+    
+    func loadToolbar(legislators: [Legislator]) {
+        guard let toolbar = self.navigationController?.toolbar else {
+            print("toolbar not found")
+            return
+        }
+        
+        var items = [UIBarButtonItem]()
+        items.append(UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil))
+        
+        for i in 0..<legislators.count {
+            let legislator = legislators[i]
+            
+            let label = UILabel();
+            label.textColor = UIColor.blue
+            label.numberOfLines = 3;
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 10)
+            label.text = legislator.title + "." + "\n" + legislator.first_name + "\n" + legislator.last_name
+            label.sizeToFit()
+            
+            let callButton = UIBarButtonItem(customView: label)
+            callButton.target = self
+            callButton.action = #selector(callLegislatorFromToolbar(sender:))
+            callButton.tag = i
+            
+            items.append(callButton)
+            items.append(UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil))
+        }
+        
+        toolbar.items = items
+    }
+    
+    func callLegislatorFromToolbar(sender: UIBarButtonItem) {
+        let legislator = self.legislators[sender.tag]
+        guard let url = URL(string: "telprompt://" + legislator.phone) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
 }
 
