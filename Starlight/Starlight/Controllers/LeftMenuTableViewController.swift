fd824361aacab3fbb2e4e6a835fc789780e6c84f
markedwardmurray <markedwardmurray@gmail.com>
2016-12-03 01:42:08 -0500

    SideMenu | left menu custom cell selection color

--- Starlight/Starlight/Controllers/LeftMenuTableViewController.swift
+++ Starlight/Starlight/Controllers/LeftMenuTableViewController.swift
@@ -17,6 +17,17 @@ class LeftMenuTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.clearsSelectionOnViewWillAppear = false
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        self.tableView.selectRow(at: indexPath, animated: false, scrollPosition: .top)
     }
-
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let selectionColor = UIView() as UIView
+        selectionColor.layer.borderWidth = 1
+        selectionColor.layer.borderColor = UIColor.init(hex: "0000ff").cgColor
+        selectionColor.backgroundColor = UIColor.init(hex: "0000ff")
+        cell.selectedBackgroundView = selectionColor
+    }
 }
